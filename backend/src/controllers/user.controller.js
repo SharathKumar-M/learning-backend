c9de481e658656ff802778d0b5e860d93526b368
SHARATH KUMAR.M <sharathm6667@gmail.com>
2026-03-12 21:41:37 +0530

    make post

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -65,9 +65,29 @@ const loginUser = async (req, res) => {
     } catch (error) {
         res.status(500).json({message: 'Server error', error: error.message})
     }
+};
+
+const logoutUser = async (req, res) => {
+    try {
+        
+        const { email } = req.body;
+
+        const user = await User.findOne({
+            email
+        })
+
+        if (!user) return res.status(404).json({message: 'User not found'});
+
+        res.status(200).json({message: 'User logged out successfully'});
+
+
+    } catch (error) {
+        res.status(500).json({message: 'Server error', error})
+    }
 }
 
 export {
     registerUser,
-    loginUser
+    loginUser,
+    logoutUser
 };
